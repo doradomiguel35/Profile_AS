aea488c1e6700b6903d00679e9483b808bc99dd6
Miguel <doradomiguel35@gmail.com>
2017-11-27 23:47:26 +0800

    modified app

--- app/src/main/java/ph/edu/uic/app/First.java
+++ app/src/main/java/ph/edu/uic/app/First.java
@@ -32,14 +32,7 @@ public class First extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         init();
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
 
 
     }
--- app/src/main/java/ph/edu/uic/app/about.java
+++ app/src/main/java/ph/edu/uic/app/about.java
@@ -1,29 +1,34 @@
 package ph.edu.uic.app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class about extends AppCompatActivity {
+    public Button btn;
 
+    public void init() {
+        btn = (Button) findViewById(R.id.btnback);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent toy = new Intent(about.this, First.class);
+                startActivity(toy);
+            }
+        });
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        init();
     }
 
 }
